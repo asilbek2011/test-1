ed2899a39be9efbe9e46a2ef44133ef9ff02ee2f
Asilbek Abdurashidov <145988869+asilbek2011@users.noreply.github.com>
2023-09-26 11:54:16 +0500

    Add files via upload

---
+++ JavaScript/main/main.js
@@ -0,0 +1,35 @@
+var Ism = String(prompt("Ismizgizni Kiriting:"))
+console.log(Ism)
+var Familya = String(prompt("Familyangizni kiriting:"))
+console.log(Familya)
+var Ochistva = String(prompt("Ochstvangizni kiriting:"))
+console.log(Ochistva)
+var kun = Number(prompt("Tug'ilgan sanangizni kiriting:"))
+console.log(kun)
+var yil = Number(prompt("Tug'ilgan yilingizni kiriting:"))
+console.log(yil)
+var kasb = String(prompt("Kasbingizni kiriting:"))
+console.log(kasb)
+var hobby = String(prompt("Hobbingizni kiriting:"))
+console.log(hobby)
+var mashgulot = String(prompt("Yoqtirgan mashg'ulotingizni kiriting:"))
+console.log(mashgulot)
+var fan = String(prompt("Yoqtirgan faninggizni kiriting:"))
+console.log(fan)
+var sport = String(prompt("Yoqtirgan sport turingizni kiriting:"))
+console.log(sport)
+var tel = Number(prompt("Telefon raqmizgizni kiriting:"))
+console.log(tel)
+
+var Javob = [
+    "Ism:"+" "+Ism+ "\n" +
+    "Familiya:"+" "+Familiya+"\n"+
+    "Ochistva:"+" "+Ochistva+"\n"+
+    "Tug'ilgan sanangizni kiriting:"+" "+kun+"\n"+
+    "Tug'ilgan yilingizni kiriting:"+" "+yil+"\n"+
+    "Kasbingizni kiriting:"+" "+kasb+"\n"+
+   "Hobbingizni kiriting:"+" "+hobby+"\n"+
+   "Yoqtirgan mashg'ulotingizni kiriting:"+" "+mashgulot+"\n"+
+   "Yoqtirgan faninggizni kiriting:"+" "+fan+"\n"+
+   "Yoqtirgan sport turingizni kiriting:"+" "+sport+"\n"
+ ]
